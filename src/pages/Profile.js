e4b4d2696b71e51904187ccf8b1c5aa0d7676e9b
Ahmed Hussaini <ahmedhussaini02@yahoo.ca>
2022-10-07 11:00:41 -0400

    commit

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react';
+import Calendar from 'react-calendar';
+// import 'react-calendar/dist/Calendar.css'
+import './Profile.css';
 
 export default function Profile() {
+  const [date, setDate] = useState(new Date())
   return (
+    // <div className="profile">
+    //   <h1>Profile</h1>
+    // </div>
     <div className="profile">
-      <h1>Profile</h1>
+      <div className="calendar-container">
+        <Calendar onChange={setDate} value={date}/>
+      </div>
+      <div className="text-center">
+        Selected date: {date.toDateString()}
+      </div>
     </div>
   );
 }
